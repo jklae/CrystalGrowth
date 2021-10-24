ab5f67b632c9a05c4f584014488f61fa242d8bc3
J. K. Lee <jklee95@users.noreply.github.com>
2021-10-24 20:16:51 +0900

    file: Add submodule

--- src/dx/DX12DrawingApp.cpp
+++ src/dx/DX12DrawingApp.cpp
@@ -230,8 +230,8 @@ void DX12DrawingApp::CompileShader()
 		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 }
 	};
 
-	D3DCompileFromFile(L"shader\\vertexShader.hlsl", nullptr, nullptr, "main", "vs_5_0", 0, 0, &mvsByteCode, 0);
-	D3DCompileFromFile(L"shader\\fragShader.hlsl", nullptr, nullptr, "main", "ps_5_0", 0, 0, &mpsByteCode, 0);
+	D3DCompileFromFile(L"..\\shader\\vertexShader.hlsl", nullptr, nullptr, "main", "vs_5_0", 0, 0, &mvsByteCode, 0);
+	D3DCompileFromFile(L"..\\shader\\fragShader.hlsl", nullptr, nullptr, "main", "ps_5_0", 0, 0, &mpsByteCode, 0);
 }
 
 void DX12DrawingApp::CreatePSO()
